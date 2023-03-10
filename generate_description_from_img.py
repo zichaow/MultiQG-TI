fix ppl computation

generate_description_from_img.py
@@ -1,4 +1,4 @@
-import os, argparse, json, random
+import os, argparse, json, random, math
 from tqdm import tqdm
 import numpy as np
 import pandas as pd
@@ -20,14 +20,14 @@ parser.add_argument("--model", type=str, default="Salesforce/blip2-flan-t5-xxl",
 parser.add_argument("--dataset_dir", type=str, default="/data/zw16/dataset_ScienceQA", help="Dataset directory")
 parser.add_argument("--save_dir", type=str, default="generated_descriptions", help="Save directory")
 parser.add_argument("--prompt_style", type=int, default=0, help="Prompt style")
-parser.add_argument("--half_precision", type=bool, default=True, help="Model precision")
+parser.add_argument("--half_precision", type=lambda x: (str(x).lower() == 'true'), default=True, help="Model precision")
 # Define generation arguments: penalty_alpha, top_k, temperature, do_sample, top_p, min_length, n_samples, max_length
 parser.add_argument("--gen_mode", type=str, default="C", help="Generation mode: C for contrastive, G for greedy, N for nucleus")
 parser.add_argument("--penalty_alpha", type=float, default=0.6, help="Penalty alpha")
 parser.add_argument("--top_k", type=int, default=4, help="Top k")
 parser.add_argument("--temperature", type=float, default=1, help="Temperature")
 parser.add_argument("--top_p", type=float, default=0.95, help="Top p")
-parser.add_argument("--do_sample", type=bool, default=True, help="Do sample")
+parser.add_argument("--do_sample", type=lambda x: (str(x).lower() == 'true'), default=True, help="Do sample")
 parser.add_argument("--n_samples", type=int, default=20, help="Number of samples")
 parser.add_argument("--min_length", type=int, default=30, help="Min length")
 parser.add_argument("--max_length", type=int, default=100, help="Max length")
@@ -54,12 +54,13 @@ def compute_perplexity(logits, labels):
 
 def generate(url, prompt, model, processor, gen_mode='C',
              penalty_alpha=0.6, top_k=4, temperature=1, do_sample=True, 
-             top_p=0.95, min_length=0, n_samples=10, max_length=100):
+             top_p=0.95, min_length=0, n_samples=10, max_length=100, half_prec=True):
     outputs = []
     ppls = []
     # image = Image.open(requests.get(url, stream=True).raw).convert('RGB')
     image = Image.open(url).convert('RGB')
-    inputs = processor(images=image, text=prompt, return_tensors="pt").to(device, torch.float16)
+    datatype = torch.float16 if half_prec else torch.float32
+    inputs = processor(images=image, text=prompt, return_tensors="pt").to(device, datatype)
     if gen_mode == 'C': # contrastive
         generation = model.generate(**inputs, 
                                        penalty_alpha=penalty_alpha, do_sample=do_sample, 
@@ -87,9 +88,57 @@ def generate(url, prompt, model, processor, gen_mode='C',
         valid_gen_idx = torch.where(gen!=0)[0]
         logits = torch.vstack([generation['scores'][i][idx].unsqueeze(0) for i in valid_gen_idx-1])
         ppl = compute_perplexity(logits, gen[gen!=0])
-        assert(torch.isnan(ppl) == False)
+        # set_trace()
+        # new_inputs = processor(images=image, text=gen_text, return_tensors="pt").to(device, datatype)
+        # target = gen.clone()
+        # target[target==0] = -100
+        # loss = model(**inputs, labels=target.unsqueeze(0))['loss'].item()
+        assert(torch.isnan(ppl) == False and ppl < 1e4)
         outputs.append(gen_text)
         ppls.append(ppl.item())
+    return outputs, ppls
+
+def generate_opt(url, prompt, model, processor, gen_mode='C',
+             penalty_alpha=0.6, top_k=4, temperature=1, do_sample=True, 
+             top_p=0.95, min_length=0, n_samples=10, max_length=100, half_prec=True):
+    outputs = []
+    ppls = []
+    # image = Image.open(requests.get(url, stream=True).raw).convert('RGB')
+    image = Image.open(url).convert('RGB')
+    datatype = torch.float16 if half_prec else torch.float32
+    inputs = processor(images=image, text=prompt, return_tensors="pt").to(device, datatype)
+    if gen_mode == 'C': # contrastive
+        generation = model.generate(**inputs, 
+                                       penalty_alpha=penalty_alpha, do_sample=do_sample, 
+                                       top_k=top_k, temperature=temperature, 
+                                       num_return_sequences=n_samples, 
+                                       min_length=min_length, max_length=max_length,
+                                       no_repeat_ngram_size=3,
+                                       output_scores=True, return_dict_in_generate=True)
+    if gen_mode == 'G': # greedy
+        generation = model.generate(**inputs, 
+                                       temperature=temperature, 
+                                       min_length=min_length, max_length=max_length,
+                                       no_repeat_ngram_size=3,
+                                       output_scores=True, return_dict_in_generate=True)
+    if gen_mode == 'N': #  nucleus
+        generation = model.generate(**inputs, do_sample=do_sample, 
+                                       top_p=top_p, temperature=temperature,
+                                       num_return_sequences=n_samples, 
+                                       min_length=min_length, max_length=max_length,
+                                       no_repeat_ngram_size=3,
+                                       output_scores=True, return_dict_in_generate=True)
+        
+    for idx in range(generation['sequences'].shape[0]):
+        gen = generation['sequences'][idx]
+        gen_text = processor.decode(gen, skip_special_tokens=True)
+        valid_gen_idx = torch.where(gen!=1)[0][1:]
+        logits = torch.vstack([generation['scores'][i][idx].unsqueeze(0) for i in valid_gen_idx-1])
+        ppl = compute_perplexity(logits, gen[gen!=1][1:])
+        assert(torch.isnan(ppl) == False and ppl < 1e4)
+        outputs.append(gen_text)
+        ppls.append(ppl.item())
+    
     return outputs, ppls
 
 def generate_vit(url, model, processor, tokenizer, gen_mode='C',
@@ -125,10 +174,11 @@ def generate_vit(url, model, processor, tokenizer, gen_mode='C',
     for idx in range(generation['sequences'].shape[0]):
         gen = generation['sequences'][idx]
         gen_text = tokenizer.decode(gen, skip_special_tokens=True)
-        valid_gen_idx = torch.where(gen!=0)[0]
+        valid_gen_idx = torch.where(gen!=50256)[0]
         logits = torch.vstack([generation['scores'][i][idx].unsqueeze(0) for i in valid_gen_idx-1])
-        ppl = compute_perplexity(logits, gen[gen!=0])
-        assert(torch.isnan(ppl) == False)
+        ppl = compute_perplexity(logits, gen[gen!=50256])
+        set_trace()
+        assert(torch.isnan(ppl) == False and ppl < 1e4)
         outputs.append(gen_text)
         ppls.append(ppl.item())
     return outputs, ppls
@@ -159,12 +209,16 @@ if 'blip' in args.model:
     processor = Blip2Processor.from_pretrained(args.model)
     # model_path = '/data/zw16/huggingface/hub/models--Salesforce--blip2-flan-t5-xxl/snapshots/f16db5558fe24665a0e38a71b7136ece83468d40/'
     # model = Blip2ForConditionalGeneration.from_pretrained(model_path, config=config, device_map="auto", torch_dtype=torch.float16)
-    model = Blip2ForConditionalGeneration.from_pretrained(args.model, device_map="auto", torch_dtype=torch.float16)
+    if args.half_precision:
+        model = Blip2ForConditionalGeneration.from_pretrained(args.model, device_map="auto", torch_dtype=torch.float16)
+    else:
+        model = Blip2ForConditionalGeneration.from_pretrained(args.model, device_map="auto", torch_dtype=torch.float32)
 elif 'vit' in args.model:
     model = VisionEncoderDecoderModel.from_pretrained(args.model)
     model.to(device)
     feature_extractor = ViTFeatureExtractor.from_pretrained(args.model)
     tokenizer = AutoTokenizer.from_pretrained(args.model)
+model.eval()
 
 #%% Generate descriptions
 set_seed(args.seed)
@@ -173,26 +227,35 @@ pids = []
 splits = []
 generated_descriptions = []
 ppls = []
-for split in ['train', 'val', 'test']:
-    for i in tqdm(range(len(pid_splits[split]))):
-        pid = pid_splits[split][i]
-        url = os.path.join(args.dataset_dir, split, pid, 'image.png')
-        if 'blip' in args.model:
-            gen_desc, ppl = generate(url, prompt, model, processor, gen_mode=args.gen_mode,
-                                 penalty_alpha=args.penalty_alpha, top_k=args.top_k, 
-                                 temperature=args.temperature, do_sample=args.do_sample, 
-                                 top_p=args.top_p, min_length=args.min_length, 
-                                 n_samples=args.n_samples, max_length=args.max_length)
-        elif 'vit' in args.model:
-            gen_desc, ppl = generate_vit(url, model, feature_extractor, tokenizer, gen_mode=args.gen_mode,
+with torch.no_grad():
+    for split in ['train', 'val', 'test']:
+        for i in tqdm(range(len(pid_splits[split]))):
+            pid = pid_splits[split][i]
+            url = os.path.join(args.dataset_dir, split, pid, 'image.png')
+            if 'blip' in args.model and 't5' in args.model:
+                gen_desc, ppl = generate(url, prompt, model, processor, gen_mode=args.gen_mode,
                                     penalty_alpha=args.penalty_alpha, top_k=args.top_k, 
                                     temperature=args.temperature, do_sample=args.do_sample, 
                                     top_p=args.top_p, min_length=args.min_length, 
-                                    n_samples=args.n_samples, max_length=args.max_length)
-        pids.extend([pid]*len(gen_desc))
-        splits.extend([split]*len(gen_desc))
-        generated_descriptions.extend(gen_desc)
-        ppls.extend(ppl)
+                                    n_samples=args.n_samples, max_length=args.max_length, 
+                                    half_prec=args.half_precision)
+            elif 'blip' in args.model and 'opt' in args.model:
+                gen_desc, ppl = generate_opt(url, prompt, model, processor, gen_mode=args.gen_mode,
+                                    penalty_alpha=args.penalty_alpha, top_k=args.top_k, 
+                                    temperature=args.temperature, do_sample=args.do_sample, 
+                                    top_p=args.top_p, min_length=args.min_length, 
+                                    n_samples=args.n_samples, max_length=args.max_length, 
+                                    half_prec=args.half_precision)
+            elif 'vit' in args.model:
+                gen_desc, ppl = generate_vit(url, model, feature_extractor, tokenizer, gen_mode=args.gen_mode,
+                                        penalty_alpha=args.penalty_alpha, top_k=args.top_k, 
+                                        temperature=args.temperature, do_sample=args.do_sample, 
+                                        top_p=args.top_p, min_length=args.min_length, 
+                                        n_samples=args.n_samples, max_length=args.max_length)
+            pids.extend([pid]*len(gen_desc))
+            splits.extend([split]*len(gen_desc))
+            generated_descriptions.extend(gen_desc)
+            ppls.extend(ppl)
 
 # Convert the results to pandas Dataframe and save the results as csv
 df = pd.DataFrame({'pid': pids, 'split': splits, 'generated_descriptions': generated_descriptions, 'ppls': ppls})